e174911df28c8401d07dcddccd40478acc8cc6a1
shwlstjd1128 <117911447+shwlstjd1128@users.noreply.github.com>
2024-07-16 22:27:29 +0900

    Title: 좀 더 어려운 수학 점수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int mat, eng; //A
+    int mat1, eng1; // B
+    scanf("%d %d", &mat, &eng);
+    scanf("%d %d", &mat1, &eng1);
+
+    if(mat > mat1 && eng > eng1){
+        printf("A");
+    }
+    else if(mat < mat1 && eng <eng1 ){
+        printf("B");
+    }
+    else if(mat == mat1 && eng > eng1){
+        printf("A");
+    }
+    else{
+        printf("B");
+    }
+
+    return 0;
+}
