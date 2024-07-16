5e6eb64f576d89e8f982cd32ad78ea1427e8de8a
shwlstjd1128 <117911447+shwlstjd1128@users.noreply.github.com>
2024-07-16 22:34:19 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/두 사람/two-person.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    char x,y;
+    scanf("%d %c", &a,&x);
+    scanf("%d %c", &b,&y);
+    if(a >= 19 && x == 'M'){
+        printf("1");
+    }
+    else if(b >= 19 && y == 'M'){
+        printf("1");
+    }
+    else if(a> b && x == y){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+    return 0;
+}
