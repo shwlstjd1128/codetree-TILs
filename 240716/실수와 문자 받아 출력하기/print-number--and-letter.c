611efcd8a4545ecdc856a86b3b8229d8953ff50a
shwlstjd1128 <117911447+shwlstjd1128@users.noreply.github.com>
2024-07-16 15:32:58 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c[10];
+    float a,b;
+    scanf("%c %f %f", c,&a,&b);
+    printf("%s\n%.2f\n%.2f",c,a,b);
+
+    return 0;
+}
