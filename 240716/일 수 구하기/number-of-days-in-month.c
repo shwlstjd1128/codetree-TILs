c4f52b83c5a5a39f3a6fe0f3957a21b6ba789d86
shwlstjd1128 <117911447+shwlstjd1128@users.noreply.github.com>
2024-07-16 23:26:21 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/일 수 구하기/number-of-days-in-month.c
+++ 240716/일 수 구하기/number-of-days-in-month.c
@@ -4,14 +4,26 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     scanf("%d", &a);
-    if(a % 2 == 1){
-        printf("31");
-    }
-    if(a % 2 == 0 && a != 2){
-        printf("30");
-    }
-    else if(a == 2){
+    if(a == 2){
         printf("28");
+    }
+    else if(a <= 7){
+        if(a % 2 != 0){
+            printf("31");
+        }
+        else{
+            printf("30");
+        }
+    }
+    else{
+        if(a > 7 && a<= 12){
+            if(a%2 != 0){
+                printf("30");
+            }
+            else{
+                printf("31");
+            }
+        }
     }
     return 0;
 }
