e917aa3693484b513513d79ad00c9c7837d078e4
shwlstjd1128 <117911447+shwlstjd1128@users.noreply.github.com>
2024-07-29 11:42:29 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
+++ 240729/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -1,19 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    int n;
-    int a[100];
-    scanf("%d", &n);
-    int idx = 0;
-    for(int i = 0; i<n; i++){
-        scanf("%d", &a[i]);
-        if(a[i] == 2){
-            idx++;
-        }
-        if(idx == 3){
-            break;
-        }
-    }
-    printf("%d", idx);
-    return 0;
+	int n;
+	int count = 0;
+	int target = 2;
+
+	// 첫 번째 줄에서 정수의 개수 n 입력 받기
+	scanf("%d", &n);
+
+	int arr[100];
+
+	// 두 번째 줄에서 n개의 숫자 입력 받기
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	// 배열을 순회하며 2의 등장 횟수 세기
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == target) {
+			count++;
+			if (count == 3) {
+				// 3번째 등장했을 때 위치 출력
+				printf("%d\n", i + 1);
+				return 0;
+			}
+		}
+	}
+
+	return 0;
 }
